9b7205eaae15b4211db96cda87044b04ac401ea4
Dylan Cunningham <gdylanc@gmail.com>
2017-10-09 16:36:05 -0700

    delete a gift idea

--- routes/gifts.js
+++ routes/gifts.js
@@ -12,11 +12,24 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
   console.log('entered in post', req.body)
-  knex('gifts').insert({creator_id: req.body.creator_id, giftee_id: req.body.giftee_id, idea: req.body.idea, idea_notes: null, is_past: null, date_given: null}).returning("*").then(data => {
+  knex('gifts').insert({creator_id: req.body.creator_id, giftee_id: req.body.giftee_id, idea: req.body.idea, idea_notes: req.body.idea_notes, is_past: null, date_given: null}).returning("*").then(data => {
     knex('gifts').select().then(gifts=> res.send(gifts))
-
   })
 })
+
+router.post('/:id/delete', function(req, res, next) {
+  console.log("entered into delete post", req.body)
+  knex('gifts').where('id', req.params.id).del().returning("*").then(data => {
+    knex('gifts').select().then(gifts=> res.send(gifts))
+  })
+})
+
+
+  // router.post('/:id/delete', (req, res, next) => {
+  //   knex('comments').where('id', req.params.id).del().then(() => {
+  //     res.json('Deleted ' + req.params.id)
+  //   })
+  // })
 
 // router.post('/:id/reply', (req, res, next) => {
 //   knex('comments').insert({content: req.body.content, comment_id: req.params.id, post_id: null}).returning('*').then(data => {
