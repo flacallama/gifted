542ba74698ff77cca5de30fc3d7af8067f423e9e
Dylan Cunningham <gdylanc@gmail.com>
2017-10-10 08:18:17 -0700

    impoved migrations/seeds with event occassions on the gift table

--- migrations/20171008174434_gifts.js
+++ migrations/20171008174434_gifts.js
@@ -7,8 +7,20 @@ exports.up = function(knex, Promise) {
     table.string('idea_notes');
     table.boolean('is_past');
     table.string('date_given');
-    table.boolean('received');
+    table.boolean('received').defaultTo( false );
+    table.boolean('birthday').defaultTo( false );
+    table.boolean('xmas').defaultTo( false );
+    table.boolean('hanukka').defaultTo( false );
+    table.boolean('anniversary').defaultTo( false );
+    table.boolean('valentines').defaultTo( false );
+    table.boolean('mothersday').defaultTo( false )
+    table.boolean('fathersday').defaultTo( false );
+    table.boolean('graduation').defaultTo( false );
+    table.boolean('barmitzvah').defaultTo( false );
+    table.boolean('bahmitzvah').defaultTo( false );
+    table.string('other_occasion');
     table.timestamp('created_at').default(knex.fn.now());
+
 
   })
 };
--- seeds/gifts.js
+++ seeds/gifts.js
@@ -5,14 +5,14 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('gifts').insert([
-        {creator_id: 1, giftee_id: 1, idea: "new shoes", idea_notes: 'mom likes shoes', is_past: false, date_given: null, received: false},
+        {creator_id: 1, giftee_id: 1, idea: "new shoes", idea_notes: 'mom likes shoes', is_past: true, date_given: null, xmas: true},
         {creator_id: 1, giftee_id: 1, idea: "mink stoll", idea_notes: 'mom likes soft things', is_past: true, date_given: '12/25/2016', received: false},
-        {creator_id: 1, giftee_id: 1, idea: "iPhone 10", idea_notes: 'iPhone 4s is scratched', is_past: false, date_given: null, received: true},
+        {creator_id: 1, giftee_id: 1, idea: "iPhone 10", idea_notes: 'iPhone 4s is scratched', is_past: false, date_given: null, received: true, mothersday: true},
         {creator_id: 1, giftee_id: 2, idea: "Horse", idea_notes: 'Dressage is an emerging interest', is_past: true, date_given: 12/25/12, received: false},
         {creator_id: 1, giftee_id: 2, idea: "Bag Pipes", idea_notes: 'Dad has been wanting a counter balance to moms violin', is_past: false, date_given: null, received: false},
-        {creator_id: 1, giftee_id: 2, idea: "Necktie", idea_notes: 'Ill never wear this', is_past: true, date_given: 12/25/12, received: true},
+        {creator_id: 1, giftee_id: 2, idea: "Necktie", idea_notes: 'Ill never wear this', is_past: true, date_given: 12/25/12, received: true, graduation: true},
         {creator_id: 1, giftee_id: 3, idea: "itune gift card", idea_notes: 'Thats what everyone wants, right?', is_past: false, date_given: null, received: false},
-        {creator_id: 1, giftee_id: 3, idea: "certificate to Olive Garde ", idea_notes: 'He said to enjoy the breadscticks', is_past: true, date_given: 12/25/12, received: true},
+        {creator_id: 1, giftee_id: 3, idea: "certificate to Olive Garde ", idea_notes: 'He said to enjoy the breadscticks', is_past: true, date_given: 12/25/12, received: true, birthday: true},
 
       ]);
     });
