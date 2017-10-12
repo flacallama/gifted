38b2901649a5558b47d07867aee06deff8071040
Dylan Cunningham <gdylanc@gmail.com>
2017-10-11 17:31:40 -0700

    fixed seeds and some post issues

--- routes/giftees.js
+++ routes/giftees.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
   console.log('entered in post add', req.body)
-  knex('giftees').insert({creator_id: req.body.creator_id, first_name: req.body.first_name, last_name: req.body.last_name, relationship: req.body.relationship, budget: parseInt(req.body.budget), email: req.body.email, age: req.body.age, birthday: req.body.birthday, birthdate: req.body.birthdate, xmas: req.body.xmas, hanukka: req.body.hanukka, mothersday: req.body.mothersday, fathersday: req.body.fathersday, valentines: req.body.valentines, anniday: req.body.anniday, annidate: req.body.annidate}).returning("*").then(data => {
+  knex('giftees').insert({creator_id: req.body.creator_id, first_name: req.body.first_name, last_name: req.body.last_name, relationship: req.body.relationship, budget: req.body.budget, email: req.body.email, age: req.body.age, birthday: req.body.birthday, birthdate: req.body.birthdate, xmas: req.body.xmas, hanukka: req.body.hanukka, mothersday: req.body.mothersday, fathersday: req.body.fathersday, valentines: req.body.valentines, anniday: req.body.anniday, annidate: req.body.annidate}).returning("*").then(data => {
     knex('giftees').select().then(giftees=> res.send(giftees))
   })
 })
