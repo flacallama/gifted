c362054ea8a400f297cb6faa7e57dca8f2795814
Dylan Cunningham <gdylanc@gmail.com>
2017-10-24 20:47:36 -0700

    fixed cors

--- app.js
+++ app.js
@@ -22,11 +22,11 @@ app.set('view engine', 'jade');
 
 
 
-//// THIS ENABLES CORS 
+//// THIS ENABLES CORS
 //enable cors?
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
