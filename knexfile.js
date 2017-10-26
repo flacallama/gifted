788cb9d11fbee7188cb8ffefc2de9ea42afaeeaa
Dylan Cunningham <gdylanc@gmail.com>
2017-10-26 12:55:50 -0700

    deploy

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,8 @@ module.exports = {
     client: 'pg',
     connection: {
       database: 'gifted',
-      host: 'https://gifted-q3-project.herokuapp.com/'
+      // host: 'https://gifted-q3-project.herokuapp.com/'
+      host: '127.0.0.1'
     },
     seeds: {
       directory: './seeds'
